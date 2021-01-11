e696fded09ad48edf1d5862221e7e7010a1d0505
Jacques Diec <jacques.diec@gmail.com>
2021-01-10 17:24:06 -0800

    links and styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :authors
+  get 'home/index'
+  root to: 'readers/home#index'
+  get '/blog/:id' => 'readers/posts#show', as: :blog_post
+
   resources :elements
   scope module: 'authors' do
     resources :posts do
       resources :elements
     end
   end
-  devise_for :authors
-  get 'home/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'readers/home#index'
 end
